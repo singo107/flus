b0f6ee8a4981d52a29e56a53ec90cfd02d71fb41
zhouxing <singo107@qq.com>
2013-10-29 22:55:46 +0800

    freemarker

--- account/account-web/src/main/java/cn/flus/account/web/controller/IndexController.java
+++ account/account-web/src/main/java/cn/flus/account/web/controller/IndexController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 /**
  * Homepage
@@ -15,10 +16,14 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/")
 public class IndexController extends BaseController {
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public ModelAndView index(ModelMap model) {
-        model.addAttribute("msg", "Hello world!");
-        return new ModelAndView("index");
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView root(ModelMap model) {
+        return new ModelAndView(new RedirectView("index"));
     }
 
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public ModelAndView index(ModelMap model) {
+        model.addAttribute("msg", "Singo Hello world!");
+        return new ModelAndView("index");
+    }
 }
---
+++ account/account-web/src/main/webapp/WEB-INF/template/index.ftl
@@ -0,0 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Home Page</title>
+</head>
+
+<body>
+	${msg}...
+</body>
+</html>
