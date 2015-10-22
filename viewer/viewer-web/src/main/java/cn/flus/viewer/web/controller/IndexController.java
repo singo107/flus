8aed7370284a2ded0caca85d6825a7cf598dcaa5
zhoux-b <zhoux-b@grandsoft.com.cn>
2015-10-22 11:33:40 +0800

    sth

--- viewer/viewer-web/src/main/java/cn/flus/viewer/web/controller/IndexController.java
+++ viewer/viewer-web/src/main/java/cn/flus/viewer/web/controller/IndexController.java
@@ -1,11 +1,19 @@
 package cn.flus.viewer.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+
+import cn.flus.viewer.core.service.AuthenticationService;
+import cn.flus.viewer.core.service.FileUploadService;
 
 /**
  * 首页
@@ -16,6 +24,12 @@ import org.springframework.web.servlet.view.RedirectView;
 @RequestMapping("/")
 public class IndexController {
 
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private FileUploadService     fileUploadService;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView root(ModelMap model) {
         return new ModelAndView(new RedirectView("index"));
@@ -24,5 +38,16 @@ public class IndexController {
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public ModelAndView index(ModelMap model) {
         return new ModelAndView("index");
+    }
+
+    @RequestMapping(value = "/preview", method = RequestMethod.GET)
+    public ModelAndView preview(@RequestParam(value = "urn", required = true) String urn, HttpServletRequest request,
+                                HttpServletResponse response, ModelMap model) {
+
+        String token = authenticationService.fetchAccessToken();
+        model.addAttribute("token", token);
+
+        model.addAttribute("urn", urn);
+        return new ModelAndView("preview");
     }
 }
---
+++ viewer/viewer-web/src/main/webapp/WEB-INF/template/preview.ftl
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>模型浏览</title>
+<link href="${static_path}/css/reset.css" rel="stylesheet" type="text/css"/>
+<link href="${static_path}/css/global.css" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" href="https://developer.api.autodesk.com/viewingservice/v1/viewers/style.css" type="text/css">
+<script type="text/javascript" src="${static_path}/js/jquery/jquery-1.11.1.min.js"></script>
+<script src="https://developer.api.autodesk.com/viewingservice/v1/viewers/viewer3D.min.js"></script>
+<script>
+function initialize() {
+
+	var options = {
+		'document' : 'urn:${urn}',
+		'env':'AutodeskProduction',
+		'getAccessToken': getToken,
+		'refreshToken': getToken,
+	};
+	
+	var viewerElement = document.getElementById('viewer');
+	var viewer = new Autodesk.Viewing.Viewer3D(viewerElement, {});
+	Autodesk.Viewing.Initializer(options,function() {
+		viewer.initialize();
+		loadDocument(viewer, options.document);
+	});
+}
+
+function getToken() {
+	return '${token}';
+}
+
+function loadDocument(viewer, documentId) {
+
+	Autodesk.Viewing.Document.load(documentId, function(doc) {
+		
+		var geometryItems = [];
+		geometryItems = Autodesk.Viewing.Document.getSubItemsWithProperties(doc.getRootItem(), {
+			'type' : 'geometry',
+			'role' : '3d'
+		}, true);
+		
+		if (geometryItems.length > 0) {
+			viewer.load(doc.getViewablePath(geometryItems[0]));
+		}
+	}, function(errorMsg) {
+		alert("Load Error: " + errorMsg);
+	});
+}
+</script>
+</head>
+
+<body onload="initialize()">
+	<div id="viewer" style="position:absolute; width:100%; height:100%;"></div>
+</body>
+</html>
